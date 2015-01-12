b1d594d87f559fb783aa147927054d74dc0bc275
Robin Ricard <ricard.robin@gmail.com>
2015-01-12 17:47:24 +0100

    4 procs

--- kernel.c
+++ kernel.c
@@ -6,23 +6,34 @@
 #include "fb.h"
 
 
-void funcA() {
+void ledOn() {
   int cptA = 0;
   while(1) {
     cptA++;
     hw_led_on();
-    drawRed();
   }
 }
 
-void funcB() {
+void ledOff() {
   int cptB = 0;
   while(1) {
     cptB++;
     hw_led_off();
+  }
+}
+
+void red() {
+  while(1) {
+    drawRed();
+  }
+}
+
+void blue() {
+  while(1) {
     drawBlue();
   }
 }
+
 
 bool fb_init() {
   FramebufferInitialize();
@@ -36,8 +47,10 @@ bool kinit() {
     vmem_setup() &&
     kalloc_setup() &&
     fb_init() &&
-    sched_new_proc(funcA, NULL, STACK_SIZE, 99) &&
-    sched_new_proc(funcB, NULL, STACK_SIZE, 95) &&
+    sched_new_proc(ledOn, NULL, STACK_SIZE, 99) &&
+    sched_new_proc(ledOff, NULL, STACK_SIZE, 95) &&
+    sched_new_proc(red, NULL, STACK_SIZE, 99) &&
+    sched_new_proc(blue, NULL, STACK_SIZE, 95) &&
     sched_start();
 }
 
