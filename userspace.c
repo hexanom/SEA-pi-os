caa4d6d0b2a1477e4f80359eb47aaa0c5f91019a
Robin Ricard <ricard.robin@gmail.com>
2015-01-12 09:01:30 +0100

    RM userspace

--- userspace.c
+++
@@ -1,9 +0,0 @@
-#include "userspace.h"
-
-void simple_init_system() {
-  // TODO: use fork() and start other processes
-  int i = 0;
-  while(true) {
-    i = i + 1;
-  }
-}
--- userspace.h
+++
@@ -1,8 +0,0 @@
-#ifndef USERSPACE_H
-#define USERSPACE_H
-
-#include "types.h"
-
-/** A simple init system, invoked by the kernel */
-void simple_init_system();
-#endif
