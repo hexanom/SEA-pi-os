9bfd3ce98b3382fa600a89873aabae321b265f7d
Robin Ricard <ricard.robin@gmail.com>
2015-01-12 17:39:54 +0100

    Play with framebuffer

--- kernel.c
+++ kernel.c
@@ -3,31 +3,39 @@
 #include "kalloc.h"
 #include "sched.h"
 #include "syscall.h"
+#include "fb.h"
+
 
 void funcA() {
   int cptA = 0;
   while(1) {
     cptA++;
     hw_led_on();
-    syscall_wait(10);
+    drawRed();
   }
 }
 
 void funcB() {
   int cptB = 0;
   while(1) {
-    cptB+=2;
-    syscall_wait(5);
+    cptB++;
     hw_led_off();
-    syscall_wait(10);
+    drawBlue();
   }
 }
+
+bool fb_init() {
+  FramebufferInitialize();
+  return true;
+}
+
 
 
 bool kinit() {
   return hw_init() &&
     vmem_setup() &&
     kalloc_setup() &&
+    fb_init() &&
     sched_new_proc(funcA, NULL, STACK_SIZE, 99) &&
     sched_new_proc(funcB, NULL, STACK_SIZE, 95) &&
     sched_start();
